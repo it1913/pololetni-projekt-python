8a1db7016a007af6326627997264c14614d8cf41
it1913 <66723368+it1913@users.noreply.github.com>
2022-01-18 10:17:09 +0100

    4. Tření a akcelerace

--- main.py
+++ main.py
@@ -32,6 +32,9 @@ class Ship(object):
         self.h = self.img.get_height()
         self.x = SCREEN_WIDTH//2
         self.y = SCREEN_HEIGHT//2
+        self.movement = False
+        self.xx = 0
+        self.yy = 0
         self.angle = 0
         self.rotationSurf = pygame.transform.rotate(self.img, self.angle)
         self.rotationRect = self.rotationSurf.get_rect()
@@ -62,15 +65,25 @@ class Ship(object):
         self.head= (self.x + self.cosine + self.w//2, self.y - self.sine * self.h//2)
 
     def moveForward(self):
-        self.x += self.cosine *2
-        self.y -= self.sine *2
+        if self.movement is True:
+            self.xx += self.cosine * 0.1
+            self.yy -= self.sine * 0.1
+        elif self.movement is False and (self.xx < 0.0 or self.yy < 0.0):
+            self.xx -= self.xx * 0.05
+            self.yy -= self.yy * 0.05
+        elif self.movement is False and (self.xx > 0.0 or self.yy > 0.0):
+            self.xx -= self.xx * 0.05
+            self.yy -= self.yy * 0.05
+        print(self.xx)
+        # print(self.yy)
+        self.x += self.xx
+        self.y += self.yy
         self.rotationSurf = pygame.transform.rotate(self.img, self.angle)
         self.rotationRect = self.rotationSurf.get_rect()
         self.rotationRect.center = (self.x, self.y)
         self.cosine = math.cos(math.radians(self.angle + 90))
         self.sine = math.sin(math.radians(self.angle + 90))
         self.head = (self.x + self.cosine + self.w // 2, self.y - self.sine * self.h // 2)
-        self.movement = True
 
 
 
@@ -87,6 +100,7 @@ run= True
 
 while run:
     clock.tick(FPS)
+    player.moveForward()
     if not gameover:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
@@ -94,13 +108,27 @@ while run:
         if keys[pygame.K_RIGHT]:
             player.turnRight()
         if keys[pygame.K_UP]:
-                player.moveForward()
+            player.movement = True
+            # print(player.movement)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    player.movement = False
+
+                    # print(player.movement)
+            elif event.type == pygame.QUIT:
+                run = False
+        # keys = pygame.key.get_pressed()
+        # if keys[pygame.K_LEFT]:
+        #     player.turnLeft()
+        # if keys[pygame.K_RIGHT]:
+        #     player.turnRight()
 
 
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+
+
+
 
     redrawGameWindow()
 pygame.quit()
