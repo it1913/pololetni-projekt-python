b0cfbc7efe715b320b82acb1de7d80d84343a0e6
it1913 <66723368+it1913@users.noreply.github.com>
2022-01-19 22:23:27 +0100

    7. Generace a kolize
    Přidání generace asteroidů a kolize střel s asteroidy.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import math
+import random
 import pygame
 
 FPS = 60
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 800
 playerBullet = []
+asteroid = []
+count = 0
+MIN_ASTEROID_SPEED = 2
+MAX_ASTEROID_SPEED = 3
 
 bg = pygame.image.load('assets/background.jpg')
 spaceShip = pygame.image.load('assets/spaceship.png')
 rocket = pygame.image.load('assets/rocket.png')
+midAsteroid = pygame.image.load('assets/midAsteroid.png')
 
 pygame.display.set_caption('Asteroids')
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -113,7 +119,7 @@ class Bullet():
 
     def draw(self, window):
         window.blit(self.shipRotation, self.shipRotationRect)
-
+    #Ničení střel, které letí pryč z hracího pole
     def borderCollision(self):
         if self.x < 0 - self.bWidth:
             return True
@@ -127,30 +133,89 @@ class Bullet():
 
 class Asteroid():
     def __init__(self):
-        pass
+        self.img = midAsteroid
+        self.aWidth = self.img.get_width()
+        self.aHeight = self.img.get_height()
+        self.randomPoint = random.choice([(random.randrange(0, SCREEN_WIDTH-self.aWidth), random.choice([-1*self.aHeight - 5, SCREEN_HEIGHT + 5])), (random.choice([-1*self.aWidth - 5, SCREEN_WIDTH + 5]), random.randrange(0, SCREEN_HEIGHT - self.aHeight))])
+        self.x, self.y = self.randomPoint
 
+        if self.x < SCREEN_WIDTH//2:
+            self.xDirection = random.random()
+        else:
+            self.xDirection = random.random() * -1
+
+        if self.x < SCREEN_HEIGHT//2:
+            self.yDirection = random.random()
+        else:
+            self.yDirection = random.random() * -1
+        self.xVelocity = self.xDirection * random.randrange(MIN_ASTEROID_SPEED, MAX_ASTEROID_SPEED)
+        if self.xVelocity > 1:
+            self.xVelocity = 2
+        elif self.xVelocity < -1:
+            self.xVelocity = -2
+        self.yVelocity = self.yDirection * random.randrange(MIN_ASTEROID_SPEED, MAX_ASTEROID_SPEED)
+        if self.yVelocity > 1:
+            self.yVelocity = 2
+        elif self.yVelocity < -1:
+            self.yVelocity = -2
+
+
+    def draw(self, window):
+        window.blit(self.img,(self.x, self.y))
+
+    def borderCollision(self):
+        if self.x < 0 - self.aWidth:
+            return True
+        elif self.x > SCREEN_WIDTH + self.aWidth:
+            return True
+
+        if self.y < 0 - self.aHeight:
+            return True
+        elif self.y > SCREEN_HEIGHT + self.aHeight:
+            return True
 
 player = Ship()
 
 def redrawGameWindow():
-    window.blit(bg, (0,0))
+    window.blit(bg, (0, 0))
     player.draw(window)
+    for a in asteroid:
+        a.draw(window)
     for b in playerBullet:
         b.draw(window)
     pygame.display.update()
 
-run= True
+run = True
 
 
 
 while run:
     clock.tick(FPS)
+    count += 1
+    # print(len(playerBullet))
+    # print(len(asteroid))
     player.moveForward()
     if not gameover:
+        if count % 50 == 0:
+            asteroid.append(Asteroid())
+
         for b in playerBullet:
             b.move()
             if b.borderCollision():
                 playerBullet.pop(playerBullet.index(b))
+
+        for a in asteroid:
+            a.x += a.xVelocity
+            a.y += a.yVelocity
+            if a.borderCollision():
+                asteroid.pop(asteroid.index(a))
+
+            #Kolize střel a asteroidů
+            for b in playerBullet:
+                if(b.x >= a.x and b.x <= a.x + a.aWidth) or b.x + b.bWidth >= a.x and b.x + b.bWidth <= a.x + a.aWidth:
+                    if(b.y >= a.y and b.y <= a.y + a.aHeight) or b.y + b.bHeight >= a.y and b.y + b.bHeight <= a.y + a.aHeight:
+                        playerBullet.pop(playerBullet.index(b))
+                        asteroid.pop(asteroid.index(a))
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
@@ -159,7 +224,7 @@ while run:
             player.turnRight()
         if keys[pygame.K_UP]:
             player.movement = True
-            # print(player.movement)
+
         for event in pygame.event.get():
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
@@ -167,21 +232,8 @@ while run:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     playerBullet.append(Bullet())
-
-                    # print(player.movement)
             elif event.type == pygame.QUIT:
                 run = False
-        # keys = pygame.key.get_pressed()
-        # if keys[pygame.K_LEFT]:
-        #     player.turnLeft()
-        # if keys[pygame.K_RIGHT]:
-        #     player.turnRight()
-
-
-
-
-
-
 
     redrawGameWindow()
 pygame.quit()
